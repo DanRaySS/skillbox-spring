be6f89678bbdb97d57e11c7e0214f07b4a4565a2
Daniil (DanRay) <danilmux@gmail.com>
2023-10-03 20:16:27 +0500

    second_task

--- base/mod1/src/SecondTask.java
+++ base/mod1/src/SecondTask.java
@@ -6,10 +6,16 @@ public class SecondTask {
     public static void main(String[] args) {
         int[][] x = { {20, 34, 2}, {9, 12, 18}, {3, 4, 5} };
 
+        int minEl = 9999;
+
         for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j <= i; j++) {
-//                System.out.println(i[j]);
+            for (int j = 0; j < x[i].length; j++) {
+                if (x[i][j] <  minEl) {
+                    minEl = x[i][j];
+                }
             }
         }
+        System.out.println(minEl);
+
     }
 }
