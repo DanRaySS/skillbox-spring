08ac495d28cfbdf43330a8f4d675f647d9c97023
Daniil <113042616+DanRaySS@users.noreply.github.com>
2023-12-11 13:10:24 +0500

    mod3_update

---
+++ buns/mod3/src/test/java/hibernate_test/Mod3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hibernate_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
